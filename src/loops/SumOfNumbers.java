72d284625f205506aee9ad239d1cbf4c7943c973
vanemoige <vanemoige8@gmail.com>
2026-09-23 17:30:28 +0100

    sumOfNumbers

---
+++ src/loops/SumOfNumbers.java
@@ -0,0 +1,15 @@
+package Loops;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=0;i<10;i++) {
+			
+			sum=sum+i;
+		}
+		System.out.println(sum);
+
+	}
+
+}
